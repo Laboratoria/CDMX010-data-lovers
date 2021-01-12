e6f882b6d5541795532834fe765a637656b95167
MiriamMVentura <miriamartv@gmail.com>
2021-01-12 14:18:16 -0600

    HU1 imprimiendo nombres en pantalla, esqueleto html y css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import { example } from './data.js';
+//import data from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+import data from './data/pokemon/pokemon.js';
+console.log(data.pokemon);
+
+const buttonStart = document.getElementById('btnStart');
+buttonStart.addEventListener('click', showNames);
+
+let pokemonName = [];
+let pokemonNum = [];
+let pokemonImag = [];
+
+//imprimiendo en pantalla nombre del primer Pokemón
+/*function showNames() {
+  const allPokemon = data.pokemon;
+  let firstPokemon = allPokemon[2];
+  console.log(firstPokemon)
+  document.getElementById('pokedexContainer').innerHTML = firstPokemon.name
+}*/
+
+//imprimiendo en pantalla los nombres de todos los Pokémon
+function showNames() {
+  const allPokemon = data.pokemon;
+  for (let i = 0; i < allPokemon.length; i++) {
+    pokemonName[i] =  allPokemon[i].name;
+  }
+  console.log(pokemonName)
+  document.getElementById('pokedexContainer').innerHTML = pokemonName;
+}
