520c8cd6914c3c989db9601a2eace228a31f4b38
osiris <wendyosi25@gmail.com>
2021-01-14 14:54:33 -0600

    Modificacion de filtros y ordenado,ya estan funcionando correctamente

--- src/data.js
+++ src/data.js
@@ -2,30 +2,36 @@ export function templateCard(propiedades) {
   return `<div class="cards" data-id="${propiedades.id}">
     <div class="card" >
       <div class="card-image">
-        <img src="${propiedades.image}" />
+        <img  class="btnmodal" src="${propiedades.image}" />
       </div>
       <div class="details">
         <p class="card-name"><h2>${propiedades.name}</h2></p>
         <p class="card-status">Estatus: ${propiedades.status}</p>
         <p class="card-specie">Especie: ${propiedades.species}</p>
-        <p class="card-origin">Origen: ${propiedades.origin.name}</p>
         <p class="card-gender">Genero: ${propiedades.gender}</p>
+        <p class="card-origin">Origen: ${propiedades.origin.name}</p>
       </div>
     </div>
   </div>`;
 }
-//recibe parametros hace calculos y retorna un resultado
-export function filterHumans(characters) {
-  const humans = characters.filter((character) => {
-    //retorna un true o false
-    return character.species === "Human";
-  });
-  return humans;
+
+
+export function order_desc(characters) {
+  const desc =characters.sort((a,b)=>{
+    if (a.name<b.name)return 1;
+    if (a.name>b.name)return -1;
+    return 0;
+  })
+  return desc;
 }
 
-export function filterAliens(characters) {
-  const aliens = characters.filter((character) => {
-    return character.species === "Alien";
-  });
-  return aliens;
+export function order_asc(characters) {
+  const asc =characters.sort((a,b)=>{
+    if (b.name<a.name)return 1; 
+    if (b.name>a.name)return -1;
+    return 0;
+  })
+  return asc;
 }
+ 
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import data from "./data/rickandmorty/rickandmorty.js";
-import { filterHumans, templateCard, filterAliens } from "./data.js";
+import { templateCard, order_desc, order_asc } from "./data.js";
 
 //recibe listado de los personajes
 function renderCards(list) {
@@ -12,10 +12,46 @@ function renderCards(list) {
 }
 //renderiza todos
 renderCards(data.results);
+//FILTRADO 
+const raceElements = document.getElementsByName('race');
+let dataFiltered;
+
+for (let i = 0; i < raceElements.length; i++) {    
+  raceElements[i].addEventListener('click', function (e) {
+    const value = e.target.dataset.value;    
+    if (value ==="Human" || value ==="Alien") {
+      let newArraySpecie = data.results.filter( character => character.species === value);
+      dataFiltered = newArraySpecie;
+      return renderCards(newArraySpecie);
+    }
+    if (value==="Female"|| value ==="Male"){
+     let newArrayGender = data.results.filter(character => character.gender === value );
+     dataFiltered = newArrayGender;
+      return renderCards(newArrayGender);
+    }
+    if (value==="Alive"|| value ==="Dead"){
+      let newArrayStatus = data.results.filter(character => character.status ===value);
+      dataFiltered=newArrayStatus;
+      return renderCards(dataFiltered);
+    }
+  return  dataFiltered;
+  
+  
+  });  
+}
+
+
+/*const array = origin;
+let repetidos = {};
+array.forEach(function(numero){
+  repetidos[numero] = (repetidos[numero] || 0) + 1;
+});*/
 
 //boton y funcionalidad del menu
 const drop_btn = document.querySelector(".drop-menu");
+const drop_btn_order = document.querySelector(".drop-menu-order");
 const menu_wrapper = document.querySelector(".wrapper");
+const menu_wrapper_order = document.querySelector(".wrapper-order");
 const menu_bar = document.querySelector(".menu-bar");
 const specie_drop = document.querySelector(".specie-drop");
 const gender_drop = document.querySelector(".gender-drop");
@@ -26,8 +62,14 @@ const location_items = document.querySelector(".location-items");
 const back_species = document.querySelector(".back-species");
 const back_gender = document.querySelector(".back-gender");
 const back_location = document.querySelector(".back-location");
+let desButton = document.getElementById('desc');
+let ascButton = document.getElementById('asc');
+
 drop_btn.onclick = () => {
   menu_wrapper.classList.toggle("show");
+};
+drop_btn_order.onclick = () => {
+  menu_wrapper_order.classList.toggle("show");
 };
 species_items.onclick = () => {
   menu_bar.style.marginLeft = "-400px";
@@ -60,20 +102,24 @@ back_location.onclick = () => {
   location_drop.style.display = "none";
 };
 
-btn.addEventListener("click", showMenu);
+//VENTANA MODAL
+let modal = document.getElementById("myModal");
+let btn = document.querySelector(".btnmodal");
+let span = document.getElementsByClassName("close")[0];
 
-//filtrado de humanos
-const btnHumans = document.getElementById("humans");
-//filtrar humanos del arreglo original
-btnHumans.addEventListener("click", function () {
-  const humans = filterHumans(data.results);
-  renderCards(humans);
-});
-//filtrado de aliens
-const btnAliens = document.getElementById("aliens");
-btnAliens.addEventListener("click", function () {
-  const aliens = filterAliens(data.results);
-  renderCards(aliens);
-});
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
-function mostrarFiltros() {}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+desButton.addEventListener('click', () => renderCards(order_desc(dataFiltered)));
+ascButton.addEventListener('click', () => renderCards(order_asc(dataFiltered)));
