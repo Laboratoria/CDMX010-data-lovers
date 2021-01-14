562350202fcdf75b6b134fc4042b6f08538e4cd1
Brenda Garcia <brendagar88@gmail.com>
2021-01-13 22:59:56 -0600

    Modificación interfaz feedback

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 
-import { infoEachPokePrinc, setClass} from './infoData.js';
+import { infoEachPokePrinc, infoEachPoke} from './infoData.js';
 import {searchPokemon} from './data.js';
 import data from './data/pokemon/pokemon.js';
 //import pokemon from './data/pokemon/pokemon.js';
-//console.log(data.pokemon);
+console.log(data.pokemon);
+//document.getElementById("home-main--button").addEventListener('click', infoPrincPokemon);
 document.getElementById("main-logo-container").style.display = 'none';
 document.getElementById("main-filter-container").style.display = 'none';
 const allPokemon = data.pokemon;
-//console.log(typeof allPokemon);
+console.log(typeof allPokemon);
 let namePokemon = [];
 let nameAllPokemon = [];
 
-//Función para mostrar todos los pokemones
 let infoPrincPokemon = document.getElementById("home-main--button");
 infoPrincPokemon.addEventListener("click", function () {
   document.getElementById("home-main-section--welcome").style.display = 'none';
@@ -25,11 +25,19 @@ infoPrincPokemon.addEventListener("click", function () {
   });
   document.getElementById('home-pokemon-info-container').innerHTML = (infoPrinPoke);
   let elements = document.getElementsByClassName('buttonEachPokeC');
-  //console.log(elements);
-  setClass(elements);
+  console.log(elements);
+  agregarClase(elements);
 })
 
-//Funciones de caja de filtros
+function agregarClase (elements) {
+  for(var i = 0; i < elements.length; i++){
+    elements[i].addEventListener('click',function(){
+    alert('Hola' + ' ' + this.value);
+    infoEachPoke(this);
+    });
+  }
+}
+
 function showCheckboxesNumber() {
   let checkboxes = document.getElementById("checkboxesNumber");
   if (!expanded) {
@@ -40,6 +48,7 @@ function showCheckboxesNumber() {
     expanded = false;
   }
 }
+
 document.getElementById("selectWeaknessesNumber").addEventListener('click', showCheckboxesNumber);
 
 function showCheckboxesType() {
@@ -52,21 +61,31 @@ function showCheckboxesType() {
       expanded = false;
     }
 }
+
 document.getElementById("selectPokemonType").addEventListener('click',  showCheckboxesType);
 
-//Función de búsqueda
 const searchPoke = document.getElementById("searchByNameButton");
+
 searchPoke.addEventListener("click", function() {
   let namePokeToSearch = document.getElementById("namePokeToSearch").value.toLowerCase();
   //console.log(namePokeToSearch);
   let pokeFounded = searchPokemon(allPokemon, namePokeToSearch);
   //console.log(pokeFounded);
+  let parentNodeEle = document.getElementById("home-pokemon-info-container");
+  function removeElementClass(parentNodeElement, className) {
+      let childClass = parentNodeElement.getElementsByClassName(className);
+      let lenght = childClass.length;
+      for (let i = lenght-1; i >= 0; i--) {
+          childClass[i].parentNode.removeChild(childClass[i]);
+      }
+  }
+  removeElementClass(parentNodeEle, "ind-info-pokemon-container");
   let pokeFoundedInfo = infoEachPokePrinc(pokeFounded[0]);
   console.log(pokeFounded[0]);
   document.getElementById('home-pokemon-info-container').innerHTML = pokeFoundedInfo;
   let container = document.getElementById('home-pokemon-info-container');
   let child = container.getElementsByClassName('buttonEachPokeC');
   console.log(child);
-  setClass(child);
+  agregarClase(child);
 })
 
