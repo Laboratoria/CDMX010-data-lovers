f9fccb3c22a1b48471da2657fb261df629372496
Valeri11-Vil <vilchisvaleria11@gmail.com>
2021-01-15 11:53:55 -0600

    Cambios CSS

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import { example } from './data.js';
-
-
+// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
+// import data from './data/rickandmorty/rickandmorty.js';
 
-<<<<<<< HEAD
-console.log(example, data);
-=======
 const url = './data/pokemon/pokemon.json';
 
 fetch()
@@ -15,4 +12,3 @@ fetch()
     .catch(function(){
 
     });
->>>>>>> talia
