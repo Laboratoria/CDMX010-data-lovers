be77733395ce63094b2711e968c9f0b5f00fb076
MiriamMVentura <miriamartv@gmail.com>
2021-01-15 13:07:07 -0600

    obteniendo el primer pokemon con imagen

--- src/data.js
+++ src/data.js
@@ -1,11 +1,13 @@
 // estas funciones son de ejemplo
-
+//funciónes de filtrado, orden y calculo
 
 //Capturando a Bulbasaur en consola
-
-
-
-export const catchPokemon= () => { //PARA USAR ESTA FUNCIÓN EN OTRO JS DE TIPO MODULO
+export const example= () => { //PARA USAR ESTA FUNCIÓN EN OTRO JS DE TIPO MODULO
+  //treaer la data
+  //manipular la data por fecth y manipularla
+  //¿cómo acceder a los elementos de la lista?
+  //
+ //map/filter/  acción de filtrar u ordenar
   return 'example';
 };
 
--- src/main.js
+++ src/main.js
@@ -4,27 +4,51 @@
 import data from './data/pokemon/pokemon.js';
 console.log(data.pokemon);
 
-const buttonStart = document.getElementById('btnStart');
-buttonStart.addEventListener('click', showNames);
+//variable del elemento HTML donde me mostrara el Pokémon
+let pokeDom=document.querySelector('.pokeContainer1')
 
+//para mostrar directo en el html sin crear un botón
+document.addEventListener("DOMContentLoaded",()=>{
+  let pokemons=getData()
+  setHTML(pokemons)
+})
+
+//obteniendo el 1er objeto del gran array, capturando a Bulbasaur
+const allPokemon = data.pokemon;
+
+function setHTML(poke){
+  let {img, num, name, about, type}=poke; //destructuración para extraer los objetos del arreglo
+  let html=`
+  <img src='${img}' >
+  ${num} El pokemon se llama ${name} y tambien ${about} TIPO ${type}`
+  console.log(html);
+  //implementar appenchild para hacer su tarjeta desde acá
+
+  pokeDom.innerHTML=html;
+}
+
+function getData(){
+  let firstPokemon = allPokemon[0]; // este es objeto Bulbasaur
+  console.log(allPokemon[0]);
+  return firstPokemon;
+}
+
+
+/*/////parte de botón todos los nombres
+//evento DOM
+let cardDom=document.querySelector('.pokeContainer2')
 let pokemonName = [];
-let pokemonNum = [];
-let pokemonImag = [];
 
-//imprimiendo en pantalla nombre del primer Pokemón
-/*function showNames() {
-  const allPokemon = data.pokemon;
-  let firstPokemon = allPokemon[2];
-  console.log(firstPokemon)
-  document.getElementById('pokedexContainer').innerHTML = firstPokemon.name
-}*/
+const buttonStart = document.getElementById('btnStart');
+buttonStart.addEventListener('click', showAllNames);
 
-//imprimiendo en pantalla los nombres de todos los Pokémon
-function showNames() {
+function showAllNames() {
   const allPokemon = data.pokemon;
+  console.log(allPokemon)
   for (let i = 0; i < allPokemon.length; i++) {
     pokemonName[i] =  allPokemon[i].name;
+
   }
   console.log(pokemonName)
-  document.getElementById('pokedexContainer').innerHTML = pokemonName;
-}
+  cardDom.innerHTML = pokemonName;
+}*/
