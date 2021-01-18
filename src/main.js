2b7e3cfa441d70a308f5ba15ff348f3c9c01953e
Valeri11-Vil <vilchisvaleria11@gmail.com>
2021-01-18 12:47:45 -0600

    Metodo Fetch

--- src/main.js
+++ src/main.js
@@ -1,24 +1,33 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+// import { example } from './data.js';
+// // import data from './data/lol/lol.js';
+//  import data from './data/pokemon/pokemon.json';
+// // import data from './data/rickandmorty/rickandmorty.js';
+// const tableElements = [document.getElementById('id-r1', 'name-r1', 'region-r1')];
+const resultado = document.getElementById('table-poke');
+// function renderPokemon(imagen) {
+//     $pokemon.setAttribute('src', imagen)
+// }
 
-<<<<<<< HEAD
-console.log(data);
-
-let filtro = data.filter(function(nombre) {
-    return filtro.name;
-})
-
-
-=======
-const url = './data/pokemon/pokemon.json';
-
-fetch()
-    .then(function(){
-
+fetch('./data/pokemon/pokemon.json')
+    .then(response => response.json())
+    .then(data => {
+        // debugger
+        // renderPokemon(data.img)
+        let pokemones = data.pokemon;
+        renderData(pokemones);
     })
-    .catch(function(){
 
+const renderData = (pokemones) => {
+    pokemones.forEach((index) => {
+        let dataPokemones = `
+       <td><img  src="${index.img}" height="100"/></td>
+       <td>${index.num}</td>
+       <td>${index.name}</td>
+       <td>${index.generation.name}</td>
+       <td class="fila1" id="details-r1">
+            <a class="img"><img id="detail" src="img/pokebola.png"/></a>
+        </td>
+        `;
+        resultado.insertAdjacentHTML("beforeend", dataPokemones);
     });
->>>>>>> develop
+}
