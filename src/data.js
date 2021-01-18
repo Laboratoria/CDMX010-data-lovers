4b19112e39748b2cd8108dae894aa22b13dbefe3
Jael91 <jaelorea@gmail.com>
2021-01-18 15:14:33 -0600

    js dinamico para crear contenedor de carta y jalar informacion de pokemon

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 // estas funciones son de ejemplo
-
+/*
 export const example = () => {
-  /*example.filter(lugar => example.name === "kanto")*/
+  /*example.filter(lugar => example.name === "kanto")
   const datos = require ('./pokemon/pokemon.js');
   fetch(datos)
     .then((res) => {
@@ -20,7 +20,7 @@ export const example = () => {
       pokemon['nombre'] = pokemon['nombre'] + ', ' + nombre.name
       });
       console.log(pokemon);
-      */
+      
     })
   return 'example';
 };
@@ -29,7 +29,43 @@ export const anotherExample = () => {
   return 'OMG';
 };
 
+*/
 
 /*MI COMENTARIO
 let region = data.filter(lugar => lugar.name === "kanto")
 console.log(region)*/
+
+
+
+
+/*
+export const example = () => {
+  /*example.filter(lugar => example.name === "kanto")
+  const datos = require ('./pokemon/pokemon.js');
+  fetch(datos)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data)=> {
+      console.log(data);
+
+      /* OPCION 1
+      const pokemon = {};
+      pokemon['name'] = data.name;
+      */
+
+      /* OPCION 2
+      pokemon.name.forEach(nombre => {
+      pokemon['nombre'] = pokemon['nombre'] + ', ' + nombre.name
+      });
+      console.log(pokemon);
+      
+    })
+  return 'example';
+};
+
+export const anotherExample = () => {
+  return 'OMG';
+};
+
+*/
--- src/main.js
+++ src/main.js
@@ -1,11 +1,34 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+//import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-console.log(example, data);
+console.log(data);
 
-/* MI COMENTARIO
-let region = data.filter(lugar => lugar.name === "kanto")
-console.log(region)*/
+// ESTA FUNCION CREA LAS TARJETAS DE CADA POKEMON DESDE JS ${pokemon.img}
+function createCard(pokemon) {
+  let card = `
+    <div class="pokemon-card">
+      <img src="" alt="">
+      <div class="circulo"></div>   
+      <h5 class="pokemon-id">${pokemon.id}</h5>
+      <h5 class="pokemon-nombre">${pokemon.name}</h5>
+      <h5 class="tipo">${pokemon.type}</h5>
+    </div>
+  `;
 
+  return card;
+};
+
+
+// ESTA FUNCION COLOCA INFORMACION DE CADA POKEMON DESDE JS 
+function setCards() {
+  let containerCards = document.getElementById('contenedor-pokemon');
+  let emptyCard = '';
+  data.pokemon.forEach(poke => emptyCard += createCard(poke));
+
+  containerCards.innerHTML = emptyCard;
+};
+
+setCards();
+
+// tarea: domContentLoaded
+// tarea: window.addEventListener
 
