e6c3d6b1fe41849fa9e0dc4ae0ead8be10a9c05f
Talia Gonzalez <taliagonzalez.dla@gmail.com>
2021-01-19 10:00:22 -0600

    Se agregó function sort compare

--- src/main.js
+++ src/main.js
@@ -1,18 +1,39 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 
 
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 
-<<<<<<< HEAD
-console.log(example, data);
-=======
+const searchResult = document.getElementById("tbody");
 const url = './data/pokemon/pokemon.json';
 
-fetch()
-    .then(function(){
-
-    })
-    .catch(function(){
-
+let pokedex = fetch (url)
+    .then((response)=> response.json ())
+    .then((data) => {
+    let pokedex = data.pokemon;
+     pokedex.sort(function compare (a, b) {
+        if (a.name < b.name
+        ) return -1;
+        if (a.name > b.name
+        ) return 1;
+        return 0;
     });
->>>>>>> talia
+    console.log(pokedex);
+    })
+
+function dataResults(pokedex){
+    
+    console.log(pokedex);
+}
+    /*const dataResults = (pokedex) => {
+    pokedex.forEach((element) => {
+    let dataPokedex = `
+    <td><img src="${element.img}"/></td>
+    <td>${element.num}</td>
+    <td>${element.name}</td>
+    <td>${element.generation.name}</td>
+    <td class="fila1" id="details-r1">
+     <a class="img"><img id="detail" src="img/pokebola.png"/></a>
+    </td> 
+    `;
+
+   searchResult.insertAdjacentHTML ("beforeend", dataPokedex)*/
