86c68b751661e73480fc3a0cc0d65f0b8ebbbed8
KarinaKano13 <75265602+KarinaKano13@users.noreply.github.com>
2021-01-19 16:02:26 -0600

    showing the data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,39 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import data from './data/rickandmorty/rickandmorty.js';
+
+const characters = data.results;
+console.log(characters[0].image)
+
+// TODO: Crear una función que retorne una tarjeta
+function createCard (character) {
+    //template strings
+    let card = `
+        <div>
+            <img src="${character.image}"></img>
+            <p>${character.name}</p>
+            <p>${character.status}</p>
+            <p>${character.type}</p>
+            <p>${character.gender}</p>
+            <p>${character.origin.name}</p>
+            <p>${character.location.name}</p>
+           
+        </div>
+    `;
+
+    return card;
+}
+
+// TODO: Función que muestra lás tarjetas
+
+function renderCards() {
+    let container = document.getElementById('container-characters');
+    let html = '';
+    // characters.forEach(character => html += createCard(character));
+    for (let i = 0; i < characters.length; i++) {
+        html += createCard(characters[i])
+    }
+
+    container.innerHTML = html;
+}
+
+renderCards();
