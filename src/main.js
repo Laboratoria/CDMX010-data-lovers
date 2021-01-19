88b4855669957cddd878891c2cdc92186b65ed14
Valeri11-Vil <vilchisvaleria11@gmail.com>
2021-01-19 10:21:10 -0600

    Cambios main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 //  import data from './data/pokemon/pokemon.json';
 // // import data from './data/rickandmorty/rickandmorty.js';
 // const tableElements = [document.getElementById('id-r1', 'name-r1', 'region-r1')];
-const resultado = document.getElementById('table-poke');
+// const resultado = document.getElementById('table-poke');
 // function renderPokemon(imagen) {
 //     $pokemon.setAttribute('src', imagen)
 // }
@@ -11,23 +11,26 @@ const resultado = document.getElementById('table-poke');
 fetch('./data/pokemon/pokemon.json')
     .then(response => response.json())
     .then(data => {
-        // debugger
+         debugger
+         console.log(data);
+         let pokemones = data.pokemon;
+         console.log(pokemones);
         // renderPokemon(data.img)
-        let pokemones = data.pokemon;
-        renderData(pokemones);
+        // let pokemones = data.pokemon;
+        // renderData(pokemones);
     })
 
-const renderData = (pokemones) => {
-    pokemones.forEach((index) => {
-        let dataPokemones = `
-       <td><img  src="${index.img}" height="100"/></td>
-       <td>${index.num}</td>
-       <td>${index.name}</td>
-       <td>${index.generation.name}</td>
-       <td class="fila1" id="details-r1">
-            <a class="img"><img id="detail" src="img/pokebola.png"/></a>
-        </td>
-        `;
-        resultado.insertAdjacentHTML("beforeend", dataPokemones);
-    });
-}
+// const renderData = (pokemones) => {
+//     pokemones.forEach((index) => {
+//         let dataPokemones = `
+//        <td><img  src="${index.img}" height="100"/></td>
+//        <td>${index.num}</td>
+//        <td>${index.name}</td>
+//        <td>${index.generation.name}</td>
+//        <td class="fila1" id="details-r1">
+//             <a class="img"><img id="detail" src="img/pokebola.png"/></a>
+//         </td>
+//         `;
+//         resultado.insertAdjacentHTML("beforeend", dataPokemones);
+//     });
+// }
