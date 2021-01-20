c9216299e49d062e4fd792fade88e8d0acf74762
Alicia Flores <floresalvarado.alicia@gmail.com>
2021-01-19 22:22:58 -0600

    Primer historia de usuario resuelta imprimiendo todos los datos en pantalla

--- src/main.js
+++ src/main.js
@@ -1,6 +1,50 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import theAthletes from './data/athletes/athletes.js';
+let losAtletas=theAthletes.athletes;
+//console.log(theAthletes.athletes[0].name)
+const butShowAll= document.getElementById("buttonShowAll");
+butShowAll.addEventListener ("click", function showCards() {
+    
+let cardsFunction = losAtletas.map((e)=>{
+    return `
+    <div class="cardAthletes">
+    <img class="logochico" src="assets/logo rio de janeiro.png">
+    <h1 class="name">Name: ${e.name}</h1>
+    <h2>Gender: ${e.gender}</h2>
+    <h2>Height: ${e.height}</h2>
+    <h2>Weight: ${e.weight}</h2>
+    <h2>Sport: ${e.sport}</h2>
+    <h2>Team: ${e.team}</h2>
+    <h2>Age: ${e.age}</h2>
+    <h2>Event: ${e.event}</h2>
+    <h2>Medal: ${e.medal}</h2>
+    </div>
+    `
+}).join(" ");
+document.getElementById("resultados").innerHTML= cardsFunction
+});
 
-console.log(example, data);
+const buttonSearch3 = document.getElementById("buttonsearch2")
+buttonSearch3.addEventListener("click", function teamFiltered() {
+let teamUser=document.getElementById("teamInput").value;
+console.log(teamInput)
+let filterTeam= losAtletas.filter(x => x.team == teamUser)
+let teamFiltered= filterTeam.map((e)=>{
+return `
+    <div class="cardAthletes">
+    <img class="logochico" src="assets/logo rio de janeiro.png">
+    <h1 class="name">Name: ${e.name}</h1>
+    <h2>Gender: ${e.gender}</h2>
+    <h2>Height: ${e.height}</h2>
+    <h2>Weight: ${e.weight}</h2>
+    <h2>Sport: ${e.sport}</h2>
+    <h2>Team: ${e.team}</h2>
+    <h2>Age: ${e.age}</h2>
+    <h2>Event: ${e.event}</h2>
+    <h2>Medal: ${e.medal}</h2>
+    </div>
+    `
+}).join(" ");
+document.getElementById("resultados").innerHTML= teamFiltered
+});
+
+
