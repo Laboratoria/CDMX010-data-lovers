d6b5dbd903a51837b555f1bf1304a485483f2385
Alicia Flores <floresalvarado.alicia@gmail.com>
2021-01-19 22:26:30 -0600

    first user story

--- src/main.js
+++ src/main.js
@@ -23,28 +23,5 @@ let cardsFunction = losAtletas.map((e)=>{
 document.getElementById("resultados").innerHTML= cardsFunction
 });
 
-const buttonSearch3 = document.getElementById("buttonsearch2")
-buttonSearch3.addEventListener("click", function teamFiltered() {
-let teamUser=document.getElementById("teamInput").value;
-console.log(teamInput)
-let filterTeam= losAtletas.filter(x => x.team == teamUser)
-let teamFiltered= filterTeam.map((e)=>{
-return `
-    <div class="cardAthletes">
-    <img class="logochico" src="assets/logo rio de janeiro.png">
-    <h1 class="name">Name: ${e.name}</h1>
-    <h2>Gender: ${e.gender}</h2>
-    <h2>Height: ${e.height}</h2>
-    <h2>Weight: ${e.weight}</h2>
-    <h2>Sport: ${e.sport}</h2>
-    <h2>Team: ${e.team}</h2>
-    <h2>Age: ${e.age}</h2>
-    <h2>Event: ${e.event}</h2>
-    <h2>Medal: ${e.medal}</h2>
-    </div>
-    `
-}).join(" ");
-document.getElementById("resultados").innerHTML= teamFiltered
-});
 
 
