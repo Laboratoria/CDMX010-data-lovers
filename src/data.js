62322ec80b74284b7a168e5d2885763e352aac54
blancadennis <lblancadennis@gmail.com>
2021-01-25 10:43:40 -0600

    Drop-down menu ready, style done

--- src/data.js
+++ src/data.js
@@ -1,10 +1,6 @@
+
+
+//console.log("hola mundo")
+
 // estas funciones son de ejemplo
 
-export const example = () => {
-  console.log('ejemplo')
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
