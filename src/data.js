1e45994ca3db0f0fbf75a098671d86abb5858066
Mariliz G <marilizgonzalez1@gmail.com>
2021-01-26 14:00:08 -0600

    cards changes

--- src/data.js
+++ src/data.js
@@ -1,14 +1,16 @@
 // estas funciones son de ejemplo
 
 export const rickandmorty = () => {
-  
-
-
+ 
   return 'rickandmorty';
 };
+
+
 
 
 export const anotherExample = () => {
   //let 
   return 'OMG';
 };
+
+const prubeba1 
